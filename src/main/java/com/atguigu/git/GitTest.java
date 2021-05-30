7b326d7e6fc91a7fae7cec61240aaa2ca83c3450
xuyang <xuyang@183.com>
2021-05-30 21:03:32 +0800

    push test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,4 +1,5 @@
 package com.atguigu.git;
+// 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
 
 public class GitTest {
     public static void main(String[] args) {
@@ -10,5 +11,7 @@ public class GitTest {
         System.out.println("hot-fix 分支进行修改！2021年5月30日 16:32:48");
         System.out.println("master 分支进行修改！2021年5月30日 16:37:08");
         System.out.println("hot-fix 分支进行修改！2021年5月30日 16:36:12");
+
+        System.out.println("push code!");
     }
 }
